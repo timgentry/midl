0cb01c1bb530ea8a820269ae732d87853c8bdd06
Tim Gentry <timgentry@users.noreply.github.com>
2021-05-11 00:44:44 +0100

    Added Midl.root

--- lib/midl.rb
+++ lib/midl.rb
@@ -4,6 +4,10 @@ require 'midl/version'
 require 'midl/parser'
 
 module Midl
+  def self.root
+    ::File.expand_path('..', __dir__)
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
