231b44d8060ebaf654a2894f4d0c4f4f0cda643a
Tim Gentry <timgentry@users.noreply.github.com>
2021-05-11 21:59:53 +0100

    Boyscouting

--- lib/midl.rb
+++ lib/midl.rb
@@ -3,11 +3,11 @@
 require 'midl/version'
 require 'midl/parser'
 
+# Midl namespacing definition
 module Midl
   def self.root
     ::File.expand_path('..', __dir__)
   end
 
   class Error < StandardError; end
-  # Your code goes here...
 end
