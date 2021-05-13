71d05621cdc45f134659b704facb48539c4044d6
Tim Gentry <timgentry@users.noreply.github.com>
2021-05-13 08:25:46 +0100

    Added the cohort name position

--- lib/midl/nodes/cohort.rb
+++ lib/midl/nodes/cohort.rb
@@ -2,7 +2,7 @@ module Midl #:nodoc: all
   module Nodes
     module Cohort
       def meta_data_item
-        { 'cohort' => { Midl::EQUALS => nic_code.text_value.upcase.sub('_', '-') } }
+        { 'cohort' => { Midl::EQUALS => text_value.upcase.sub('_', '-'), position: interval } }
       end
     end
   end
--- test/cohort_test.rb
+++ test/cohort_test.rb
@@ -4,14 +4,14 @@ class CohortTest < Minitest::Test
   def test_should_capture_the_cohort
     parser = Midl::Parser.new('all patient records in study NIC-345678')
     assert parser.valid?
-    assert_equal({ Midl::EQUALS => 'NIC-345678' }, parser.meta_data['cohort'])
+    assert_equal({ Midl::EQUALS => 'NIC-345678', position: 29...39 }, parser.meta_data['cohort'])
 
     parser = Midl::Parser.new('all patient records in cohort NIC_345678')
     assert parser.valid?
-    assert_equal({ Midl::EQUALS => 'NIC-345678' }, parser.meta_data['cohort'])
+    assert_equal({ Midl::EQUALS => 'NIC-345678', position: 30...40 }, parser.meta_data['cohort'])
 
     parser = Midl::Parser.new('all patient records in study cohort NIC_345678, but do not apply opt-outs')
     assert parser.valid?
-    assert_equal({ Midl::EQUALS => 'NIC-345678' }, parser.meta_data['cohort'])
+    assert_equal({ Midl::EQUALS => 'NIC-345678', position: 36...46 }, parser.meta_data['cohort'])
   end
 end
